ed047799f9a8cf36aca410ee4e78dd6e022eda1c
Dwigth Astacio <dwigth32@outlook.es>
2024-04-16 10:01:42 -0600

    feat(movement): move towards the model direction

--- Scripts/CharacterMovement.cs
+++ Scripts/CharacterMovement.cs
@@ -13,9 +13,8 @@ public partial class CharacterMovement : CharacterBody3D
 	private Node3D m_Model;
 	private AnimationTree m_AnimationTree;
 	private AnimationNodeStateMachinePlayback m_AnimationState;
-	private float m_RotationSpeed = 0.01f;
+	private float m_Rotationm_Speed = 0.01f;
 	private bool m_Jumping = false;
-	private float m_TurnAmount;
 	private Array m_Attacks = new()
 	{
 		"1H_Melee_Attack_Slice_Diagonal",
@@ -24,8 +23,9 @@ public partial class CharacterMovement : CharacterBody3D
 	};
 
 	private bool m_LastFloor = true;
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	private bool m_IsAttacking = false;
+	public const float m_Speed = 5.0f;
+	public const float m_JumpVelocity = 4.5f;
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
@@ -40,18 +40,14 @@ public partial class CharacterMovement : CharacterBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y -= gravity * (float)delta;
 
-		//Debug.WriteLine($"{m_LastFloor} {IsOnFloor()}");
-
-
 		// Handle Jump.
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
-			velocity.Y = JumpVelocity;
+			velocity.Y = m_JumpVelocity;
 			m_Jumping = true;
 			m_AnimationTree.Set("parameters/conditions/jumping", true);
 			m_AnimationTree.Set("parameters/conditions/grounded", false);
@@ -73,27 +69,26 @@ public partial class CharacterMovement : CharacterBody3D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-
 		m_CamForward = (m_Cam.GlobalBasis.Z * new Vector3(1, 0, 1)).Normalized();
-		m_Move = inputDir.Y * m_CamForward + inputDir.X * m_Cam.GlobalTransform.Basis.X;
+		//CharacterMovement should be set by the model forward
+		m_Move = Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
 
-		if (m_Move.Length() > 1) m_Move = m_Move.Normalized();
-		//Debug.WriteLine(m_Move.Length());
 		if (m_Move != Vector3.Zero)
 		{
-			velocity.X = m_Move.X * Speed;
-			velocity.Z = m_Move.Z * Speed;
-			Vector3 velocityBlenPosition = velocity * m_Model.Transform.Basis;
-			Debug.WriteLine(new Vector2(velocityBlenPosition.X, -velocityBlenPosition.Z) / Speed);
-			m_AnimationTree.Set("parameters/IWR/blend_position", new Vector2(velocityBlenPosition.X, -velocityBlenPosition.Z) / Speed);
+			velocity.X = m_Move.X * m_Speed;
+			velocity.Z = m_Move.Z * m_Speed;
+
+			m_Model.LookAt(m_Move + Position);
+
+			Vector3 velocityBlenPosition = velocity * Transform.Basis;
+			m_AnimationTree.Set("parameters/IWR/blend_position", new Vector2(velocityBlenPosition.X, -velocityBlenPosition.Z) / m_Speed);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-			m_TurnAmount = 0;
-			Vector3 velocityBlenPosition = velocity * m_Model.Transform.Basis;
-			m_AnimationTree.Set("parameters/IWR/blend_position", new Vector2(velocityBlenPosition.X, -velocityBlenPosition.Z) / Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, m_Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, m_Speed);
+			Vector3 velocityBlenPosition = velocity * Transform.Basis;
+			m_AnimationTree.Set("parameters/IWR/blend_position", new Vector2(velocityBlenPosition.X, -velocityBlenPosition.Z) / m_Speed);
 		}
 
 		Velocity = velocity;
@@ -103,14 +98,21 @@ public partial class CharacterMovement : CharacterBody3D
 		{
 			Transform3D transform = m_Model.Transform;
 			Vector3 axis = transform.Basis.GetEuler();
-			axis.Y = Mathf.LerpAngle(m_Model.Rotation.Y, m_Move.Y, (float)(m_RotationSpeed * delta));
+			axis.Y = Mathf.LerpAngle(m_Model.Rotation.Y, m_Move.Y, (float)(m_Rotationm_Speed * delta));
 			transform.Basis = new Basis(axis, 0.01f) * transform.Basis;
 			m_Model.Transform = transform;
 		}
 
 		if (Input.IsActionPressed("attack"))
 		{
-			m_AnimationState.Travel((StringName)m_Attacks.PickRandom());
+			m_IsAttacking = true;
+		}
+
+		while (m_IsAttacking)
+		{
+			StringName attack = (StringName)m_Attacks.PickRandom();
+			m_AnimationState.Travel(attack);
+			m_IsAttacking = false;
 		}
 	}
 }
